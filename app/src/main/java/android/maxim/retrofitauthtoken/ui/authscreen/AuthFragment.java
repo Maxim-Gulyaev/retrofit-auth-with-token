bea0b5e153e756a185f3175a24dc3b50e36ad8e1
MaximGuljaev <maximuspo4ta@gmail.com>
2023-05-12 00:05:48 +0300

    add credentials check

--- app/src/main/java/android/maxim/retrofitauthtoken/ui/authscreen/AuthFragment.java
+++ app/src/main/java/android/maxim/retrofitauthtoken/ui/authscreen/AuthFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -42,16 +43,18 @@ public class AuthFragment extends Fragment {
                 view1 -> NavHostFragment.findNavController(AuthFragment.this)
                 .navigate(R.id.action_AuthFragment_to_ProductsFragment));
 
-
-        //TODO Delete hardcode
         binding.btnEnter.setOnClickListener(v -> {
-            authViewModel.auth("dpettegre6",
-                    "YVmhktgYVS"
-                    /*binding.tvUsername.getText().toString(),
-                    binding.tvFirstName.getText().toString()*/
-            );
-            showUserData();
-            turnOnButtonNext();
+            String userName = binding.tvUsername.getText().toString();
+            String password = binding.tvPassword.getText().toString();
+
+            if(userName.length() != 0 && password.length() != 0) {
+                authViewModel.auth(userName, password);
+                showUserData();
+                turnOnButtonNext();
+            } else {
+                Toast.makeText(getContext(), R.string.fill_credentials, Toast.LENGTH_LONG)
+                        .show();
+            }
         });
     }
 
