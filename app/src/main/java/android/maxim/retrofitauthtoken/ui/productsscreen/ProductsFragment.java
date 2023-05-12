165f45e44f66d4574a9c1564f04e0979a100c3c1
MaximGuljaev <maximuspo4ta@gmail.com>
2023-05-12 22:19:59 +0300

    add error handling in productsFragment

--- app/src/main/java/android/maxim/retrofitauthtoken/ui/productsscreen/ProductsFragment.java
+++ app/src/main/java/android/maxim/retrofitauthtoken/ui/productsscreen/ProductsFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -21,12 +22,12 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 @AndroidEntryPoint
 public class ProductsFragment extends Fragment {
 
-    private FragmentProductsBinding binding;
-    private AuthViewModel authViewModel;
     @Inject
     CompositeDisposable compositeDisposable;
     @Inject
     Api api;
+    private FragmentProductsBinding binding;
+    private AuthViewModel authViewModel;
 
     @Override
     public View onCreateView(
@@ -45,25 +46,28 @@ public class ProductsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    private void initRecycler() {
+        authViewModel.token.observe(getViewLifecycleOwner(), token -> compositeDisposable.add(
+                Single.just(token)
+                        .subscribeOn(Schedulers.io())
+                        .flatMap(it -> api.getProducts(token))
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(it -> {
+                            binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                            binding.recyclerView.setAdapter(new ProductsAdapter(it.products, getContext()));
+                        }, this::showError)
+        ));
+    }
+
+    private void showError(Throwable error) {
+        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG)
+                .show();
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    private void initRecycler() {
-        authViewModel.token.observe(getViewLifecycleOwner(), token -> {
-            compositeDisposable.add(
-                    Single.just(token)
-                            .subscribeOn(Schedulers.io())
-                            .flatMap(it -> api.getProducts(token))
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(it -> {
-                                binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                                binding.recyclerView.setAdapter(new ProductsAdapter(it.products, getContext()));
-                            })
-            );
-        });
     }
 
     @Override
